dbed15040c23c6fa92cdc8f4a66cd04ad6af617b
nick <146023067+ikoeuo@users.noreply.github.com>
2024-04-17 05:29:59 -0400

    done :D

--- CollectionApp.py
+++ CollectionApp.py
@@ -7,43 +7,46 @@ def run():
 
     while True:
         
-        print('\n1. Create Item \n2. List Item(s) \n3. Update Item \n4. Delete Item \n5. Exit App\n')
-        choice = input('What would you like to do (1-5): ')
+        print('\n1. Create/Update Item \n2. List Item(s) \n3. Delete Item \n4. Exit App\n')
+        choice = input('What would you like to do (1-4): ')
 
         if choice == '1':
             while True:
                 category = input('\nEnter category (book/movie/song): ').lower()
                 if category in ['book', 'movie', 'song']:
+                    number = ''
+                    while number.isdigit() == False:
+                        number = input('Enter #: ')
                     title = input('Enter title: ')
                     author = input('Enter author: ')
                     genre = input('Enter genre: ')
-                    Collection.create_item(category, title, author, genre)
+                    category = category+str(number)
+                    Collection.create_item(data, category, title, author, genre)
                     break
                 else: 
-                    print('Invalid category, Enter (book/card/cd)\n')
+                    print('\nInvalid category, Enter (book/card/cd)\n')
 
         elif choice == '2':
-            category = input('Enter category (book/movie/song): ').lower()
-            Collection.list_items(category)
+                Collection.list_items(data)
 
         elif choice == '3':
-            category = input("Enter category (book/movie/song): ").lower()
-            index = int(input(f'Enter what you {category} you would like to update (enter {category} number): ')) - 1
-            title = input(f'Enter new {category} title: ')
-            author = input(f'Enter new {category} author: ')
-            genre = input(f'Enter new {category} genre: ')
-            Collection.update_item(category, index, title, author, genre)
+            while True:
+                category = input('\nEnter category (book/movie/song): ').lower()
+                if category in ['book', 'movie', 'song']:
+                    number = ''
+                    while number.isdigit() == False:
+                        number = input('Enter #: ')
+                    category = category+str(number)
+                    Collection.delete_item(data,category)
+                    break
+                else:
+                    print('\nInvalid category\n')
 
         elif choice == '4':
-            category = input('Enter category (book/movie/song): ').lower()
-            index = int(input(f'Enter {category} to delete: ')) - 1
-            Collection.delete_item(category, index)
-
-        elif choice == '5':
-            print('Thank you for using my app!')
+            print('\nThank you for using my app!')
             break
 
         else:
-            print('Invalid selection.')
+            print('\nInvalid selection.')
 
 run()
